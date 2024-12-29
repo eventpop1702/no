f9d88765cc0cb0863a3106a80b09ca4a6bfe4515
eventpop1702 <eventpopbgmi1702+qwer@gmail.com>
2024-12-29 20:09:29 +0530

    Update and rename soul (1).py to soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = "7840494232:AAGQyf6YzK"
+TOKEN = "8083989511:AAF600ruXncDoIkyti3CV-qRJsB4YlLGMJE"
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
-FORWARD_CHANNEL_ID = -1002156421934
-CHANNEL_ID = -1002156421934
-error_channel_id = -1002156421934
+FORWARD_CHANNEL_ID = -1002295550112
+CHANNEL_ID = -1002295550112
+error_channel_id = -1002295550112
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
